4c94d2b14cd94cc6643d8bda796b6b36b38f34de
irtazahashmi <irtazahashmi@gmail.com>
2024-05-05 17:07:21 +0300

    Imrpove test coverage.

--- tests/test_hodgedecomposition.py
+++ tests/test_hodgedecomposition.py
@@ -62,6 +62,16 @@ class TestHodgeDecompostion:
         L1U = sc.upper_laplacian_matrix(rank=1)
         assert np.allclose(L1U @ f_h, 0)
 
+    def test_harmonic_component_sig_fig(
+        self, sc: SimplicialComplexNetwork, flow: np.ndarray
+    ):
+        # L1@f_h = 0
+        B1 = sc.incidence_matrix(rank=1)
+        B2 = sc.incidence_matrix(rank=2)
+        f_h = get_harmonic_component(B1, B2, flow, round_fig=True)
+        L1 = B1.T @ B1
+        assert np.allclose(L1 @ f_h, 0, atol=1e-1)
+
     def test_curl_component(
         self, sc: SimplicialComplexNetwork, flow: np.ndarray
     ):
@@ -71,6 +81,15 @@ class TestHodgeDecompostion:
         L1L = sc.lower_laplacian_matrix(rank=1)
         assert np.allclose(L1L @ f_c, 0)
 
+    def test_curl_component_sig_fig(
+        self, sc: SimplicialComplexNetwork, flow: np.ndarray
+    ):
+        # L1L@f_c = 0
+        B2 = sc.incidence_matrix(rank=2)
+        f_c = get_curl_component(B2, flow, round_fig=True)
+        L1L = sc.lower_laplacian_matrix(rank=1)
+        assert np.allclose(L1L @ f_c, 0, atol=1e-1)
+
     def test_gradient_component(
         self, sc: SimplicialComplexNetwork, flow: np.ndarray
     ):
@@ -79,3 +98,12 @@ class TestHodgeDecompostion:
         f_g = get_gradient_component(B1, flow, round_fig=False)
         L1U = sc.upper_laplacian_matrix(rank=1)
         assert np.allclose(L1U @ f_g, 0)
+
+    def test_gradient_component_sig_fig(
+        self, sc: SimplicialComplexNetwork, flow: np.ndarray
+    ):
+        # L1U@f_g = 0
+        B1 = sc.incidence_matrix(rank=1)
+        f_g = get_gradient_component(B1, flow, round_fig=True)
+        L1U = sc.upper_laplacian_matrix(rank=1)
+        assert np.allclose(L1U @ f_g, 0, atol=1e-1)
